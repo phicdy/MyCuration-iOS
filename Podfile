33fe4ce332233541dde1a9c2ff90e617cdbacd86
phicdy <phicdy@gmail.com>
2015-06-23 18:01:07 +0900

    Add KIF by CocoaPod

---
+++ Podfile
@@ -0,0 +1,3 @@
+target 'MyRSSReaderTests', :exclusive => true do
+  pod 'KIF', '~> 3.0', :configurations => ['Debug']
+end
---
+++ Pods/Target Support Files/Pods-MyRSSReaderTests/Pods-MyRSSReaderTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyRSSReaderTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyRSSReaderTests
+@end
